89d2acbb6d33dfb394a206f3fce8957639c1484b
Exper <xcrewonwork@gmail.com>
2026-03-10 22:51:45 +0800

    linear-expert: type sdkRequest client

--- worker/src/linear/sdk.ts
+++ worker/src/linear/sdk.ts
@@ -2,12 +2,18 @@ import { LinearClient } from '@linear/sdk';
 
 export type LinearSdkClient = LinearClient;
 
+type RawRequestCapable = {
+  client: {
+    rawRequest: <T = unknown>(query: string, variables: Record<string, unknown>) => Promise<{ data?: T }>;
+  };
+};
+
 export function createLinearSdkClient(accessToken: string): LinearSdkClient {
   return new LinearClient({ accessToken });
 }
 
-export async function sdkRequest<T>(client: any, query: string, variables: Record<string, unknown>): Promise<T> {
+export async function sdkRequest<T>(client: RawRequestCapable, query: string, variables: Record<string, unknown>): Promise<T> {
   // Use underlying GraphQL client to avoid model hydration issues in unit tests.
-  const res = await client.client.rawRequest(query, variables);
-  return res.data as T;
+  const res = await client.client.rawRequest<unknown>(query, variables);
+  return (res.data ?? {}) as T;
 }
